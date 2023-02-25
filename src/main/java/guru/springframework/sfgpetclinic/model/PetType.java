d493720ea28b8a25bdf56e9b2dc577037698baee
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-02-25 17:57:18 +0100

    Updated owner with JPA entity mappings

--- src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,9 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
 import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
