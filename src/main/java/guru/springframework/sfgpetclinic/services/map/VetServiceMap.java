ea3fb7767634c49caf028fa6458e9a2cdd368b0e
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-02-27 20:03:28 +0100

    Implemented Map impl for Visit Service

--- src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialityService specialityService;
 
-    public VetMapService(SpecialityService specialityService) {
+    public VetServiceMap(SpecialityService specialityService) {
         this.specialityService = specialityService;
     }
 
