d5d144ff65917250f2f936eb37da526702d3c03e
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-03-06 19:14:19 +0100

    added showOwner to OwnerController to search Owners by Id and ownerDetails.html

--- src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -22,4 +22,9 @@ public class PetType extends BaseEntity {
     public PetType(String name) {
         this.name = name;
     }
+    @Override
+    public String toString() {
+        return name;
+    }
 }
+
